a97b02904d1fd6319c3fd7a9bc4ffe5ff717f7f2
Adrien099 <arbe0243@gmail.com>
2024-06-28 15:41:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ module.exports = {
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
-  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '❤️,ADRIEN',
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '𝆹𝅥𝇁𝄃𝄂 𝚫𝐃𝐑𝐈𝚵𝚷 𓂃🪽',
   ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
   LOG_MSG: toBool(process.env.LOG_MSG) || false,
   RMBG_KEY: process.env.RMBG_KEY || 'null',
